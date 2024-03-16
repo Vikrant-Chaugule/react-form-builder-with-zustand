380b905f7bd664e4f75999036aac6bbbfb13f8e6
Vikrant-Chaugule <chaugulevikrant005@gmail.com>
2024-03-16 20:24:03 +0530

    Refactored folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './form-builder-module/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
